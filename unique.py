cbfae3178eb95cfd7ba8ef58c5acc82a273b0242
Sampathk9 <sampathmiryala@sampath-mba.local>
2026-01-18 19:15:08 -0800

    first unique character

---
+++ unique.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+s = "swiss"
+
+# for ch in s:
+#     print(ch)
+
+
+def unique_characters(s):
+    if not s:
+        return None
+
+    freq ={}
+    for ch in s:
+        freq[ch] = freq.get(ch, 0) + 1
+
+    for ch in s:
+        if freq[ch] == 1:
+            return ch
+    
+    return None
+print(unique_characters(s)) # w
